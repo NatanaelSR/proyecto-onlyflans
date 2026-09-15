1a65cd7006ff4fc498220acce07eb3c770257cf7
Natanael <natanael.soto.rodriguez@gmail.com>
2026-09-14 22:14:53 -0300

    Completada Tarea 1 y 2 del Hito 3 - se trabajo en vistas index y welcome

--- web/views.py
+++ web/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Flan
 
 def indice(request):
     return render(request, 'web/index.html')
@@ -7,4 +8,18 @@ def acerca(request):
     return render(request, 'web/about.html')
 
 def bienvenido(request):
-    return render(request, 'web/welcome.html')
+    flanes_privados = Flan.objects.filter(is_private=True)
+    context = { 
+        'flanes': flanes_privados
+    }
+    return render(request, 'web/welcome.html', context)
+
+def indice(request):
+    flanes_publicos = Flan.objects.filter(is_private=False)
+    context = {
+        'flanes': flanes_publicos
+    }
+
+    return render(request, 'web/index.html', context)
+
+
