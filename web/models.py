06774ebb7290050c72def6fe1e8dc5cb30ddce3a
Felipe Gonzalez <felipe.gonzalez.gu@usach.cl>
2026-09-20 22:47:38 -0300

    hito 3-3

--- web/models.py
+++ web/models.py
@@ -12,3 +12,9 @@ class Flan(models.Model):
 
     def __str__(self):
         return self.name
+
+class ContactForm(models.Model):
+    contact_form_uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    customer_email = models.EmailField()
+    customer_name = models.CharField(max_length=64)
+    message = models.TextField()
--- web/views.py
+++ web/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Flan
+from django.shortcuts import render, redirect
+from .models import Flan, ContactForm
+from . import forms as f
 
 def indice(request):
     return render(request, 'web/index.html')
@@ -23,3 +24,15 @@ def indice(request):
     return render(request, 'web/index.html', context)
 
 
+def contacto(request):
+    if request.method == 'POST':
+        form = f.ContactFormForm(request.POST)
+        if form.is_valid():
+            contact_form = ContactForm.objects.create(**form.cleaned_data)
+            return redirect('exito')
+    else:
+        form = f.ContactFormForm()
+    return render(request, 'web/contacto.html',{'form' : form})
+
+def exito(request):
+        return render(request, 'web/exito.html',{})
